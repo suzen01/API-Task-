dc5bbe48769a41991d983030a509da193ee0d9c1
suzan <s.suzanhassan@hotmail.com>
2022-01-10 20:19:22 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import React from 'react';
 import CountriesComponent from './component/Component';
-
+import "./index.css"
 function App() {
-  return (
+   return (
     <div>
-      <h1> hello </h1>
+    
+      <h1> " LIST COUNTRIES " </h1>
 
       <CountriesComponent/>
   
--- src/component/Component.js
+++ src/component/Component.js
@@ -10,10 +10,22 @@ const CountriesComponent = () => {
         const newCountriesState = countriesArray.map((countries, index) => {
           return (
 
-            <div key={index}>
-           <div>{countries.name} , {countries.capital}</div>
-           <div>{countries.} , {countries.} </div>
-                             </div>
+           <div key={index}>
+                
+           <div>
+             <br/><br/>
+            <img src={countries.flag}
+              alt="altrnative text" 
+              style={{width: 200, height: 200, left:100 }}/>
+             <div className='name'>name:{countries.name}, </div> 
+             <div>capital: {countries.capital} ,</div>  
+             <div>region: {countries.region} ,</div> 
+              population:{countries.population}
+              <br/>
+
+           </div>
+           
+           </div>
 
           )
         });
